88ef16971222ccf190e38e5ea6e95e3caa75b318
dekdadung <dekdadung001@gmail.com>
2022-02-19 09:58:18 +0800

    perbaikan sidebar dan histori

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -6,8 +6,8 @@ $session = session();
         <a href="<?= base_url() ?>" class="navbar-brand sidebar-gone-hide">
             <img src="<?= base_url('template/assets/img/logo-utama.png') ?>" alt="" style="width:130px">
         </a>
-        <h1>Solusi Keuangan Anda</h1>
         <a href="#" class="nav-link sidebar-gone-show" data-toggle="sidebar"><i class="fas fa-bars"></i></a>
+        <h1>Solusi Keuangan Anda</h1>
         <div class="nav-collapse">
             <a href="<?= site_url('login/logout') ?>" class="btn btn-round smooth btn-icon icon-left">
                 <i class="fas fa-sign-out-alt"></i> Logout
@@ -24,7 +24,7 @@ $session = session();
             <li class="nav-item"><a href="<?= site_url('datagadai') ?>" class="nav-link sc_load">Pegadaian</a></li>
             <li class="nav-item"><a href="<?= site_url('saldo') ?>" class="nav-link sc_load">Saldo</a></li>
             <li class="nav-item"><a href="<?= site_url('datacabang') ?>" class="nav-link sc_load">Cabang</a></li>
-            <li class="nav-item dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle"
+            <li class="nav-item dropdown"><a href="#" data-toggle="dropdown" class="nav-link has-dropdown"
                     aria-expanded="false">Pengaturan</a>
                 <ul class="dropdown-menu">
                     <li><a href="<?= site_url('datanasabah') ?>" class="dropdown-item sc_load">Data
@@ -35,7 +35,7 @@ $session = session();
                     <li><a href="<?= site_url('databarang') ?>" class="dropdown-item sc_load">Jenis Barang</a></li>
                 </ul>
             </li>
-            <li class="nav-item dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle"
+            <li class="nav-item dropdown"><a href="#" data-toggle="dropdown" class="nav-link has-dropdown"
                     aria-expanded="false">Lelang</a>
                 <ul class="dropdown-menu">
                     <li><a href="<?= site_url('terlelang') ?>" class="dropdown-item sc_load">Barang TerLelang</a></li>
@@ -43,7 +43,7 @@ $session = session();
                             Lelang</a></li>
                 </ul>
             </li>
-            <li class="nav-item dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle"
+            <li class="nav-item dropdown"><a href="#" data-toggle="dropdown" class="nav-link has-dropdown"
                     aria-expanded="false">Laporan</a>
                 <ul class="dropdown-menu">
                     <li><a href="<?= site_url('laporanlunas') ?>" class="nav-link sc_load">Barang Lunas</a></li>
@@ -52,13 +52,12 @@ $session = session();
                     <li><a href="<?= site_url('laporanterlelang') ?>" class="nav-link sc_load">Barang Terlelang</a></li>
                 </ul>
             </li>
-            <li class="nav-item dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle"
+            <li class="nav-item dropdown"><a href="#" data-toggle="dropdown" class="nav-link has-dropdown"
                     aria-expanded="false">Histori</a>
                 <ul class="dropdown-menu">
-                    <li><a href="<?= site_url('laporanlunas') ?>" class="nav-link sc_load">Histori Penebusan</a></li>
-                    <li><a href="<?= site_url('laporan') ?>" class="nav-link sc_load">Histori Denda</a></li>
-                    <li><a href="<?= site_url('laporanakanlelang') ?>" class="nav-link sc_load">Histori Perpanjangan</a>
-                    </li>
+                    <li><a href="<?= site_url('historiT') ?>" class="nav-link sc_load">Histori Penebusan</a></li>
+                    <li><a href="<?= site_url('historiD') ?>" class="nav-link sc_load">Histori Denda</a></li>
+                    <li><a href="<?= site_url('historiP') ?>" class="nav-link sc_load">Histori Perpanjangan</a></li>
                 </ul>
             </li>
         </ul>
@@ -79,8 +78,8 @@ $session = session();
             </li>
             <li class="nav-item"><a href="<?= site_url('datanasabah') ?>" class="nav-link">Data
                     Nasabah</a>
-            <li class="nav-item dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle"
-                    aria-expanded="false">Laporan</a>
+            <li class="nav-item dropdown">
+                <a href="#" data-toggle="dropdown" class="nav-link has-dropdown" aria-expanded="false">Laporan</a>
                 <ul class="dropdown-menu">
                     <li><a href="<?= site_url('laporanlunas') ?>" class="nav-link sc_load">Barang Lunas</a></li>
                     <li><a href="<?= site_url('laporan') ?>" class="nav-link sc_load">Data Aktif</a></li>
@@ -88,7 +87,7 @@ $session = session();
                     <li><a href="<?= site_url('laporanterlelang') ?>" class="nav-link sc_load">Barang Terlelang</a></li>
                 </ul>
             </li>
-            <li class="nav-item dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle"
+            <li class="nav-item dropdown"><a href="#" data-toggle="dropdown" class="nav-link has-dropdown"
                     aria-expanded="false">Histori</a>
                 <ul class="dropdown-menu">
                     <li><a href="<?= site_url('historiT') ?>" class="nav-link sc_load">Histori Penebusan</a></li>
